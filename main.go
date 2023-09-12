15d176f1fdbc230ec52e0986ef09eefb95eecb88
Semyon Okulov <khidey@inbox.ru>
2023-09-13 02:31:32 +0700

    trying to fix CORS

--- main.go
+++ main.go
@@ -73,6 +73,14 @@ func handleRequest(c *gin.Context) {
 
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+
+	if c.Request.Method == "OPTIONS" {
+		c.Status(http.StatusNoContent)
+		return
+	}
 
 	if _, exists := banList[clientIP]; exists {
 		c.String(http.StatusTooManyRequests, "You are temporarily banned")
