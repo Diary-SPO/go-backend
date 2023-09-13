4ad089067194e541974d9997e116957f18347d09
Semyon Okulov <khidey@inbox.ru>
2023-09-13 18:26:41 +0700

    updates

--- main.go
+++ main.go
@@ -6,19 +6,30 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-
+	"os"
 	"time"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} [${ip}] ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		Output:     os.Stdout,
+	}))
+
 	app.Use(cors.New())
-	app.Use(logger.New())
 
 	app.Use(limiter.New(limiter.Config{
-		Expiration: 10 * time.Second,
 		Max:        10,
+		Expiration: 300 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).SendString("Rate limit exceeded.")
+		},
 	}))
 
 	app.Post("/hello", func(c *fiber.Ctx) error {
@@ -33,6 +44,7 @@ func main() {
 	routes.AddStudentLessonsRoute(app)
 
 	err := app.Listen(":3000")
+
 	if err != nil {
 		panic(err)
 	}
